9dc9fed8b180170c4ed0f355dfcdb7d7b3445942
kd2wtr <122415458+kd2wtr@users.noreply.github.com>
2023-11-20 15:24:20 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+#Finn Tidd ELA8 project
+#This is the main file for the ELA8 project.
+#It contains the main function that runs the program.
+#you should give me a 4 on this!
+import random
+def intro ():
+  print ("Welcome to BARN 8")
+  print( )
+  print( )
+  print ("May the odds be ever in your favour")
+  print( )
+  print( )
+  print( )
+def truck ():
+
+def warehousefront ():
+
+  
+def gravelroad ():
+
+def backdoor():
+def barn8():
+def office():
+
+
+intro()
+def outside ():
+  print ("You are standing north of a gigantic warehouse in a gravel parking lot.")
+  print('It is pitch dark save for the flickering glow of a lighbulb long overdue to be')
+  print('replaced.')
+  print('To the north is a gravel road.')
+  print('To the west sits your beat-up red pickup truck, off but still warm from a long')
+  print('drive.')
+
+outside()
+user = input ('->')
+if user == 'south' or 's'or 'South':
+  
+  
+
+  
